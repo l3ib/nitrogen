1c8670ad9c788cfc2eca92f131c026c4496cdba7
Javeed Shaikh <syscrash2k@gmail.com>
2009-04-08 21:01:34 -0400

    don't include png headers. this fixes a bug due to new libpng tricks.

--- src/Thumbview.cc
+++ src/Thumbview.cc
@@ -23,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "Thumbview.h"
 #include "md5.h"
 #include <glib/gstdio.h>
-#include <png.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
