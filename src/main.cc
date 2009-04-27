40cc025a7f06337a1230abcb8e0c424eb29e7d4e
Dave Foster <daf@minuslab.net>
2009-04-27 11:51:43 -0400

    Safer thread system support.

--- src/main.cc
+++ src/main.cc
@@ -77,7 +77,7 @@ int main (int argc, char ** argv) {
 	Gtk::Main kit(argc, argv);
 	Gtk::IconTheme::get_default()->append_search_path(NITROGEN_DATA_DIR
 													  G_DIR_SEPARATOR_S "icons");
-	Glib::thread_init();
+	if (!Glib::thread_supported()) Glib::thread_init();
 	Config *cfg = Config::get_instance();
 	ArgParser *parser = Util::create_arg_parser();
 
