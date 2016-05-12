8619a2b32d951ae45122f2be3603c8f6e696db42
Vladimir-csp <vladimir-csp@yandex.ru>
2016-05-12 23:55:36 +0400

    whitespace fix

--- src/Util.cc
+++ src/Util.cc
@@ -251,8 +251,8 @@ bool is_image(std::string file) {
 		file.find(".JPEG") != std::string::npos ||
 		file.find(".gif")  != std::string::npos ||
 		file.find(".GIF")  != std::string::npos ||
-		file.find (".svg") != std::string::npos ||
-        	file.find (".SVG") != std::string::npos)
+		file.find(".svg")  != std::string::npos ||
+        	file.find(".SVG")  != std::string::npos)
 		return true;
 
 	return false;
