573e356dd237b0d34836a14cb29ff649d0c59927
Johannes Kamprad <archlinux@kamprad.net>
2023-09-29 23:57:41 +0200

    Update Util.cc  add webp support

--- src/Util.cc
+++ src/Util.cc
@@ -253,7 +253,9 @@ bool is_image(std::string file) {
 		file.find(".gif")  != std::string::npos ||
 		file.find(".GIF")  != std::string::npos ||
 		file.find(".svg")  != std::string::npos ||
-        	file.find(".SVG")  != std::string::npos)
+        	file.find(".SVG")  != std::string::npos ||
+		file.find(".webp")  != std::string::npos ||
+		file.find(".WEBP")  != std::string::npos)
 		return true;
 
 	return false;
